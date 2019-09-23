8bc3b380aea9f46f5a78971920f93ed1c5c30e75
TriPham1990 <pmtri1990@gmail.com>
2019-09-23 18:12:33 +0700

    update

--- src/controllers/DictionaryController.java
+++ src/controllers/DictionaryController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class DictionaryController {
